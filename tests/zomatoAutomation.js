88d1b52c2cb880745c426a42dc27654ad9dd6278
Chelsea <chelseahannan@icloud.com>
2020-05-12 17:07:31 -0400

    add test and pageObject files and folders

---
+++ tests/zomatoAutomation.js
@@ -0,0 +1,13 @@
+
+var myPage {}
+
+module.exports = {
+    beforeEach: browser => {
+        browser.page.zomatoPage()
+        myPage.navigate()
+    },
+
+    after: browser => {
+       myPage.end() 
+    }
+}
